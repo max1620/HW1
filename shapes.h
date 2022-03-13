2f85cc32085d6a91c35dcd323e3f8f0bd6aee192
max1620 <x35713712@gmail.com>
2022-03-13 23:24:24 +0800

    first

---
+++ shapes.h
@@ -0,0 +1,52 @@
+#ifndef _SHAPES_H_
+#define _SHAPES_H_
+
+#define PI 3.1415926
+
+// Base class
+class Shape {
+public:
+	Shape(double a = 0, double b = 0) {
+		width = a; height = b;
+	}
+	~Shape() {}
+
+	void setWidth(double w) {
+		width = w;
+	}
+	void setHeight(double h) {
+		height = h;
+	}
+	virtual double area() = 0;
+
+protected:
+	double width;
+	double height;
+};
+
+class Rectangle : public Shape {
+public:
+	Rectangle(double a = 0, double b = 0) :Shape(a, b) { }
+
+	double area() {
+		return (width * height);
+	}
+};
+class Circle : public Shape {
+public:
+	Circle(double a = 0) :Shape(a) { }
+
+	double area() {
+		return (width * PI);
+	}
+};
+class Triangle : public Shape {
+public:
+	Triangle(double a = 0, double b = 0) :Shape(a, b) { }
+
+	double area() {
+		return (width * height/2);
+	}
+};
+
+#endif
